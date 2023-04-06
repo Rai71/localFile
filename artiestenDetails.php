989b0ba4c526b552873623da97d6f7693ce6ba09
TristanSkull <118739361+TristanSkull@users.noreply.github.com>
2023-04-06 13:19:49 +0200

    Update artiestenDetails.php

--- artiestenDetails.php
+++ artiestenDetails.php
@@ -15,16 +15,119 @@
     require_once('Php\header.php');
     
     ?>
-        <main id="A">
-            <article><div class="square"></div>
-            <h1>
-                Lorem ipsu  m dolor sit amet consectetur adipisicing elit. Rem numquam eum error sed temporibus, nostrum eaque laboriosam distinctio architecto delectus aperiam sapiente voluptatibus. Dolorem consequuntur facilis, veniam fugit corrupti maxime?</h1>
-        </article>
-            <h2>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt odit iste eos fuga. Adipisci vitae quam, natus tempora aperiam harum, laudantium perspiciatis facilis incidunt officiis earum animi qui consectetur culpa.
-            </h2>
-            <h3>Tristan Peters</h3>
-        </main>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Artist Details</title>
+	<style type="text/css">
+		#popup {
+			display: none;
+			position: fixed;
+			top: 50%;
+			left: 50%;
+			transform: translate(-50%, -50%);
+			padding: 20px;
+			background-color: #fff;
+			border: 1px solid #000;
+			box-shadow: 0px 0px 10px #000;
+			z-index: 9999;
+		}
+	</style>
+</head>
+<body>
+    <main>
+	<form action="" method="post">
+		<label for="artiest">Choose an artist:</label>
+		<select name="artiest" id="artiest">
+			<?php
+				$servername = "localhost";
+				$username = "username";
+				$password = "password";
+				$dbname = "database_name";
+				$conn = new mysqli($servername, $username, $password, $dbname);
+
+				if ($conn->connect_error) {
+					die("Connection failed: " . $conn->connect_error);
+				}
+
+				$sql = "SELECT naam FROM artiesten";
+				$result = $conn->query($sql);
+
+				if ($result->num_rows > 0) {
+					while($row = $result->fetch_assoc()) {
+						echo "<option value='" . $row["naam"] . "'>" . $row["naam"] . "</option>";
+					}
+				} else {
+					echo "<option value=''>No artists found</option>";
+				}
+
+				$conn->close();
+			?>
+		</select>
+		<input type="submit" name="submit" value="View Details">
+	</form>
+
+	<div id="popup">
+		<a href="#" id="close">Close</a>
+		<div id="details">
+			<?php
+				if (isset($_POST["submit"]) && isset($_POST["artiest"])) {
+					$servername = "localhost";
+					$username = "username";
+					$password = "password";
+					$dbname = "database_name";
+					$conn = new mysqli($servername, $username, $password, $dbname);
+
+					if ($conn->connect_error) {
+						die("Connection failed: " . $conn->connect_error);
+					}
+
+					$artiest = mysqli_real_escape_string($conn, $_POST["artiest"]);
+
+
+					$sql = "SELECT * FROM artiesten WHERE naam='$artiest'";
+					$result = $conn->query($sql);
+
+					if ($result->num_rows > 0) {
+						while($row = $result->fetch_assoc()) {
+							echo "<h2>" . $row["naam"] . "</h2>";
+							echo"<p><strong>Genre:</strong> " . $row["genre"] . "</p>";
+                            echo "<p><strong>Albums:</strong></p>";
+                            echo "<ul>";
+                            $sql_albums = "SELECT * FROM albums WHERE artiest='$artiest'";
+                            $result_albums = $conn->query($sql_albums);
+                            if ($result_albums->num_rows > 0) {
+                            while($row_albums = $result_albums->fetch_assoc()) {
+                            echo "<li>" . $row_albums["titel"] . " (" . $row_albums["jaar"] . ")</li>";
+                            }
+                            } else {
+                            echo "<li>No albums found</li>";
+                            }
+                            echo "</ul>";
+                            }
+                            } else {
+                            echo "<p>No details found for selected artist</p>";
+                            }
+				$conn->close();
+			}
+		?>
+	</div>
+</div>
+
+<script type="text/javascript">
+
+	var popup = document.getElementById("popup");
+	var close = document.getElementById("close");
+
+	document.getElementById("artiest").addEventListener("change", function() {
+		popup.style.display = "block";
+	});
+
+	close.addEventListener("click", function() {
+		popup.style.display = "none";
+	});
+</script>
+</main>
         <footer>
         <section>
             <p class="fp">volg onze socials!</p>
